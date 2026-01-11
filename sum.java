db159f7cbceeff9157b1711b4cabd27eab6356f5
Sanchit Kaushik <sanchitkaushik1307@gmail.com>
2026-01-11 20:13:57 +0530

    First commit

---
+++ sum.java
@@ -0,0 +1,31 @@
+/*import java.util.*;
+public class sum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int num1= sc.nextInt();
+        int num2=sc.nextInt();
+        int sum = num1+num2;
+        System.out.println(sum);
+
+    }
+}
+impot java.util.*;
+public class sum{
+    public static void main(String args[]){
+        scanner sc = new scanner (System.in);
+        int num1= sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        int avg = (num1+num2+num3)/3;
+        System.out.println(avg);
+    }
+}*/
+import java.util.*;
+public class sum{
+    public static void main(String args[]){
+        scanner sc = new scanner (system.in);
+        int side= sc.nextInt();
+        int area = side*side;
+        System.out.println(area);
+    }
+}
